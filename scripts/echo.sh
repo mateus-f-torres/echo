8d7511ebc4da5db24d45896b666e093e3966d12a
mateus-f-torres <mateusfigtorres@gmail.com>
2021-05-18 10:46:26 -0300

    feat(echo.sh): get last commit author and choosen fruit

--- scripts/echo.sh
+++ scripts/echo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-# Find location of this script file
+
 directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 project=$(cd "$directory/../" && pwd)
+file=$project/example.txt
 
-cat "$project/example.txt"
+git log -1 --pretty=format:'%an %at'
 
+grep -E "^\w+" "$file" | sed "s/.*=\([a-z]\)/\1/"
