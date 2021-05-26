971995925434d21c9f69ae15ced94c5d74f59795
mateus-f-torres <mateusfigtorres@gmail.com>
2021-05-25 21:30:32 -0300

    refactor(echo.sh): develop curl POST script

--- scripts/echo.sh
+++ scripts/echo.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-set -e
+script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+project_directory=$(cd "$script_directory/../" && pwd)
+file=$project_directory/example.txt
 
-directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-project=$(cd "$directory/../" && pwd)
-file=$project/example.txt
+user=
+date=
+hash=
+fruits=
+endpoint=
 
-git log -1 --pretty=format:'%an %at'
+get_latest_git_log_information() {
+  local git_log="$(git log -1 --pretty=format:'%an-%at-%h')"
+  local regex="(.+)-(.+)-(.+)"
 
-grep -E "^\w+" "$file" | sed "s/.*=\([a-z]\)/\1/"
+  if [[ $git_log =~ $regex ]]; then
+    user="${BASH_REMATCH[1]}"
+    date="${BASH_REMATCH[2]}"
+    hash="${BASH_REMATCH[3]}"
+  fi
+}
+
+get_user_enabled_fruits() {
+  fruits="$(grep -E "^\w+" "$file" | sed "s/.*=\([a-z]*\)/\"\1\"/" | tr -s '\n' ',')"
+  fruits="${fruits::-1}"
+}
+
+post_enabled_fruits() {
+  local data="{\"user\":\"$user\",\"date\":$date,\"commit\":\"$hash\",\"fruits\":[$fruits]}"
+  curl -X POST -H "Content-Type: application/json" -d "\"$data\"" $endpoint
+}
+
+get_latest_git_log_information
+get_user_enabled_fruits
+post_enabled_fruits
